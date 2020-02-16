0928b4c2208853a66f89b6a16fb7f062e4af74a1
zenius <zenius2050@gmail.com>
2020-02-16 18:04:49 +0530

    city Model added

---
+++ CityInfo.API/Models/City.cs
@@ -0,0 +1,9 @@
+namespace CityInfo.API.Models
+{
+  public class City
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+  }
+}
