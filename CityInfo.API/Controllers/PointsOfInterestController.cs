278ceb2a8b3d6e9e0ecb6ff292088cd3a1693a06
zenius <zenius2050@gmail.com>
2020-02-17 00:23:12 +0530

    PointsOfInterestController added

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using CityInfo.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+  [ApiController]
+  [Route("api/cities/{cityId}/pointsofinterest")]
+  public class PointsOfInterestController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult GetPointsOfInterest(int cityId)
+    {
+      var cityToReturn = CityData.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+
+      if (cityToReturn == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(cityToReturn.PointsOfInterests);
+    }
+
+    [HttpGet("{Id}")]
+    public IActionResult GetSinglePointOfInterest(int cityId, int pointOfInterestId)
+    {
+      var cityToReturn = CityData.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+
+      if (cityToReturn == null)
+      {
+        return NotFound();
+      }
+
+      var pointOfInterest = cityToReturn.PointsOfInterests.FirstOrDefault(p => p.Id == pointOfInterestId);
+
+      if (pointOfInterest == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(pointOfInterest);
+    }
+
+  }
+}
