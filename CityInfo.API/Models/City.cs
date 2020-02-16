3a261178216a09cc8456bc52f885d5cbb4b30196
zenius <zenius2050@gmail.com>
2020-02-17 00:21:54 +0530

    City Model modified

--- CityInfo.API/Models/City.cs
+++ CityInfo.API/Models/City.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace CityInfo.API.Models
 {
   public class City
@@ -5,5 +7,15 @@ namespace CityInfo.API.Models
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+
+    public int PointsOfInterestCount
+    {
+      get
+      {
+        return PointsOfInterests.Count;
+      }
+    }
+
+    public ICollection<PointsOfInterest> PointsOfInterests { get; set; } = new List<PointsOfInterest>();
   }
 }
