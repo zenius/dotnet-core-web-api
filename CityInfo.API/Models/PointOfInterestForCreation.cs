19d6e6f1930710e85990ca75b759d54ca2815477
zenius <zenius2050@gmail.com>
2020-02-19 17:57:10 +0530

    Data Annotation for validation added

--- CityInfo.API/Models/PointOfInterestForCreation.cs
+++ CityInfo.API/Models/PointOfInterestForCreation.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CityInfo.API.Models
 {
   public class PointsOfInterestForCreation
   {
+    [MaxLength(50)]
+    [Required(ErrorMessage = "Please provide a name with a maximum length of 50.")]
     public string Name { get; set; }
+
+    [MaxLength(200)]
     public string Description { get; set; }
   }
 }
